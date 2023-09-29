f44f97533583696f74a466a03e47c3b5be7232c0
Samuel <a2jsamuel@gmail.com>
2023-09-30 01:43:55 +0300

    This is to fix if typeerror for the parameters in 2-file

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -22,7 +22,11 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    this._name = name;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
   }
 
   get length() {
@@ -30,7 +34,11 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    this._length = length;
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
   }
 
   get students() {
@@ -38,6 +46,10 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    this.students = students;
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      this._students = students;
+    }
   }
 }
