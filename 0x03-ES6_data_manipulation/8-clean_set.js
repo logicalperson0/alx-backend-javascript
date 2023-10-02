55b13d052f2a4573f64d38f89911a69c6a0da0b5
Samuel <a2jsamuel@gmail.com>
2023-10-02 22:41:06 +0300

    fix and add for 8-file for non-str

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const str = [];
 
-  if (startString === '') {
+  if (startString === '' || typeof startString !== 'string') {
     return '';
   }
 
