b5eebc581a546aa7bfd86ce09a2451bb9e93d776
Dark Mind <dune934@gmail.com>
2023-10-05 01:36:06 +0300

    for task 10

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,15 @@
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Teacher.ts" />
+
+const Cpp: Subjects.Cpp = new Subjects.Cpp();
+const Java: Subjects.Java = new Subjects.Java();
+const React: Subjects.React = new Subjects.React();
+
+let cTeacher: Subjects.Teacher {
+    firstName = 'John',
+    lastName = 'Smith',
+    experienceTeachingC = 10,
+}
