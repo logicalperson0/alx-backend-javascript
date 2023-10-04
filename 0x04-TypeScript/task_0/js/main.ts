b79f2dcff8fab8afff8cde524cd4b57e2f8a9d73
Dark Mind <dune934@gmail.com>
2023-10-04 21:26:53 +0300

    adding to main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -19,4 +19,25 @@ const studentsList = [student1, student2];
 
 const table = document.createElement('table');
 
-const row1 = document.cre
+const row1 = document.createElement('tr');
+const row2 = document.createElement('tr');
+
+const data1 = document.createElement('td');
+const data2 = document.createElement('td');
+const data3 = document.createElement('td');
+const data4 = document.createElement('td');
+
+data1.innerHTML = ;
+data2.innerHTML = ;
+
+row1.appendChild(data1);
+row1.appendChild(data2);
+
+data3.innerHTML = ;
+data4.innerHTML = ;
+
+row2.appendChild(data3);
+row2.appendChild(data4);
+
+table.appendChild(row1);
+table.appendChild(row2);
