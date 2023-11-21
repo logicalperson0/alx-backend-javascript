0daa843d7f8d33a71e15c57f26c3d5e829986a61
Samuel <a2jsamuel@gmail.com>
2023-11-21 15:02:21 +0300

    fix for eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write('Your name is: ' + chunk);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
-process.on('exit', function() { 
-return console.log('This important software is now closing'); 
-});
+process.on('exit', () => console.log('This important software is now closing'));
