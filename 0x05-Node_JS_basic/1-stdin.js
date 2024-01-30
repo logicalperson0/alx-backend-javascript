6c61598a808f74b41bee711b2eab31abb4d11786
Samuel <a2jsamuel@gmail.com>
2024-01-30 14:18:59 +0300

    this is for task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', function() {
+    const naming = process.stdin.read();
+    if (naming !== null) {
+        process.stdout.write(`Your name is: ${naming}`);
+    }
+});
+
+process.stdin.on('end', function() {
+    process.stdout.write('This important software is now closing\n');
+});
