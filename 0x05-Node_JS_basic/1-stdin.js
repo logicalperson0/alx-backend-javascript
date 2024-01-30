de2ecd6e0e1ce8b514fc591d814a53e54f411d4f
Samuel <a2jsamuel@gmail.com>
2024-01-30 14:25:55 +0300

    fixing 1-file for task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,12 +2,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', function() {
-    const naming = process.stdin.read();
-    if (naming !== null) {
-        process.stdout.write(`Your name is: ${naming}`);
-    }
+  const naming = process.stdin.read();
+  if (naming !== null) {
+    process.stdout.write(`Your name is: ${naming}`);
+  }
 });
 
 process.stdin.on('end', function() {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
